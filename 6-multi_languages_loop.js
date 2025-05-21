5cdac3cc9391ec21581038d3a1fc97d2c11b5947
zakariabanid101 <banidzakaria2@gmail.com>
2025-05-21 20:45:30 +0100

    use foreach

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,9 @@
 const array= ["C is fun", "Python is cool", "JavaScript is amazing"]
 
-for(i = 0; i < array.length; i++){
-    console.log(array[i])
-}
+// for(i = 0; i < array.length; i++){
+//     console.log(array[i])
+// }
+
+array.forEach(element => {
+    console.log(element)
+});
