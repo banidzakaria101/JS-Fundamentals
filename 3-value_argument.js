9b96057b71a4ca0a533e52b1d8945a72c8164cec
zakariabanid101 <banidzakaria2@gmail.com>
2025-05-21 19:14:37 +0100

    change condition from null to undefined

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,7 @@
 const args = process.argv.slice(2);
 
 function myFunction(args) {
-  if (args ===  null) {
+  if (args ===  undefined) {
     console.log("No argument");
   }else {
     console.log(args);
