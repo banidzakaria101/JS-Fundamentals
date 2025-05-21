85ea4b549d0236b131bfa927815664d6e0fbfc39
zakariabanid101 <banidzakaria2@gmail.com>
2025-05-21 14:59:33 +0100

    add 2-arguments.js file

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+function myFunction(arg1, arg2) {
+    if(arg1 == null && arg2 == null) {
+        console.log("No argument");
+    }else if(arg1 != null && arg2 == null || arg1 == null && arg2 != null){
+        console.log("Argument found");
+    }else{
+        console.log("Arguments found");
+    }
+}
+
+myFunction(1);
