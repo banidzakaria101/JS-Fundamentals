74d2d327c995f5d53129e3cfc81b28fd80393daa
zakariabanid101 <banidzakaria2@gmail.com>
2025-05-21 22:03:26 +0100

    add 8-square.js file

---
+++ 8-square.js
@@ -0,0 +1,23 @@
+const args = process.argv.slice(2);
+const toNum = parseInt(args[0])
+
+function myFunction(x) {
+    if (isNaN(x)) {
+        console.log("Missing size");
+    } else if (x > 0) {
+        //   let output = "";
+        //   for (let i = 0; i < x; i++) {
+        //     output += "x";
+        //   }
+
+        for (let i = 0; i < x; i++) {
+            let square = '';
+            for (let j = 0; j < x; j++) {
+                square += 'x';
+            }
+            console.log(square);
+        }
+    }
+}
+
+myFunction(toNum);
