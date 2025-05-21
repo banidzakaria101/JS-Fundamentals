f1cb9355bed4c65a34c9212315ddb72b08ac9670
zakariabanid101 <banidzakaria2@gmail.com>
2025-05-21 17:35:12 +0100

    use process.argv

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,4 +1,4 @@
-function myFunction(arg1, arg2) {
+/*function myFunction(arg1, arg2) {
     if(arg1 == null && arg2 == null) {
         console.log("No argument");
     }else if(arg1 != null && arg2 == null || arg1 == null && arg2 != null){
@@ -7,6 +7,18 @@ function myFunction(arg1, arg2) {
         console.log("Arguments found");
     }
 }
+*/
 
-myFunction(1);
-myFunction();
+function myFunction(args) {
+  if (args.length === 0) {
+    console.log("No argument");
+  } else if (args.length === 1) {
+    console.log("Argument found");
+  } else {
+    console.log("Arguments found");
+  }
+}
+
+const args = process.argv;
+
+myFunction(args);
